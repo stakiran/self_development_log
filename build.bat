1c6dcab249f57cdaaf4276aeac133b90d201526c
stakiran <stakiran@users.noreply.github.com>
2019-07-07 17:57:54 +0900

    merge builder and uploder because forgetting

--- build.bat
+++ build.bat
@@ -1,6 +1,11 @@
 @echo off
 setlocal
 
+echo building...
 python %~dp0builder.py -i %~dp0log.md -o %~dp0index.html -t %~dp0template.html
+echo fin.
 
-timeout 3
+echo uploading...
+rem upload.bat is my original batchfile to do add, commit and push at one time.
+upload
+echo fin.
